a9d88ebb6e2e808ae6f4be64d658b09fe2b9c8f6
Alejandro Medina <alex.m@alexmedina.com>
2023-07-12 11:06:25 -0500

    Update streamlit_app.py
    Fixed bad syntax in header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as sl
 
 sl.title('My Parents New Healthy Diner') # title
-sl.header('Breakfast Menu) # header
+sl.header('Breakfast Menu') # header
 
 sl.text('Omega 3 & Blueberry Oatmeal')
 sl.text('Kale, Spinach & Rocket Smoothie')
