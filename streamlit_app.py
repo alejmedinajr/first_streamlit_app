93918a57d80962e292e9cdfef230fc087d02b60c
Alejandro Medina <alex.m@alexmedina.com>
2023-07-12 11:10:22 -0500

    Update streamlit_app.py
    Added emojis
    added new text for avocado toast
    added new header for smoothies

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,10 @@ import streamlit as sl
 sl.title('My Parents New Healthy Diner') # title
 sl.header('Breakfast Menu') # header
 
-sl.text('Omega 3 & Blueberry Oatmeal')
-sl.text('Kale, Spinach & Rocket Smoothie')
-sl.text('Hard-Boiled Free-Range Egg')
-          
+sl.text('🥣 Omega 3 & Blueberry Oatmeal')
+sl.text('🥗 Kale, Spinach & Rocket Smoothie')
+sl.text(' 🐔 Hard-Boiled Free-Range Egg')
+sl.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇') # smoothie header
+ 
