a26bd816d40bd1dc05230ad2d819e6a552378ac7
Alejandro Medina <alex.m@alexmedina.com>
2023-07-12 11:18:10 -0500

    Update streamlit_app.py
    Now the users know what fruit they are selecting by a better index

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,9 @@ sl.text('🥑🍞 Avocado Toast')
 sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇') # smoothie header
  
 fruits = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+
+fruit = fruit.set_index('Fruit') # so that the index of the fruit list makes sense when the readers read
+
 # ability to pick a list so that user can pick the fruit that they want to include
 sl.multiselect("Pick some fruits:", list(fruits.index))
 
